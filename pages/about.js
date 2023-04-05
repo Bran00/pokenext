f180f593113c891f87358118502f951f9e105d69
Brando Rocha <86134682+Bran00@users.noreply.github.com>
2023-04-05 17:55:55 -0300

    feat: add the page home with cards pokemons

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,7 @@ export default function About() {
   return (
     <div className={styles.about}>
       <h1>Sobre o projeto</h1>
-      <p> Construido para mostrar a variedade de personagens do anime "Pokemon" onde muitos deles não são conhecidos entre o público.
+      <p> Construido para mostrar algumas variedades de personagens do anime "Pokemon" onde muitos deles não são conhecidos entre o público.
       </p>
       <Image src="/images/charizard.png" width="300" height="300" alt="imagem do charizard do pokemon" />
     </div>
